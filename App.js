2d19764a3d68540d90c85a124d205ab73ff5496c
rajeshvinay <rajeshvinay2012@gmail.com>
2023-07-18 04:41:32 +0530

    Commit 2 for Episode-3

--- App.js
+++ App.js
@@ -1,31 +1,5 @@
-/**
- * <div id="parent">
- *  <div id="child1">
- *      <h1>I'm h1 tag</h1>
- *      <h2>I'm h2 tag</h2>
- *  </div>
-*   <div id="child2">
-*      <h1>I'm h1 tag</h1>
-*      <h2>I'm h2 tag</h2>
-*   </div>
- * </div>
- * 
- * 
- * 
- */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const heading1 = React.createElement('h1',{id:'heading1',key:'1'},"Hello Nested Block");
-const heading2 = React.createElement("h2",{id:'heading1',key:'2'},"Sibling Tag")
-
-const child1 = React.createElement("div",{id:'child',key:'c1'},[heading1,heading2])
-const child2 = React.createElement("div",{id:'child2',key:'c2'},[heading1,heading2])
-
-const parent = React.createElement("div",{id:"parent"},[child1,child2])
-
-
-
-
-
-// const heading = React.createElement('h1',{className:"heading"},"Hello world from React!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+root.render([<HeadingComp />,<Title />])
